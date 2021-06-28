f4feb5d1ecf67e4203e9f29657066d913229a5b4
Andrew Nguyen <tandrewng@gmail.com>
2021-06-28 01:43:17 -0400

    ex1 done and ex3 running

--- a3/ex1.py
+++ a3/ex1.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import random
 
 from numpy.core.numeric import empty_like
 
 class Node:
-    def __init__(self, X, y, depth, max_depth, f):
+    def __init__(self, X, y, depth, max_depth, f, random_forests):
         self.X = X
         self.y = y
         self.depth = depth
@@ -14,6 +15,7 @@ class Node:
         self.left = None
         self.right = None
         self.descision = None
+        self.random_forests = random_forests
         if (depth < max_depth and not self.isPure()): 
             # internal node, must be split
             self.split()
@@ -23,12 +25,16 @@ class Node:
         return
     
     def split(self):
-        num_features = len(self.X[0])
+        d = len(self.X[0])
+        rootd = list(range(d))
+        if (self.random_forests): 
+            num_features = round(math.sqrt(d))
+            rootd = list(random.sample(range(d), num_features))
         num_inputs = len(self.y)
         splits = np.array([])
         losses = np.array([])
 
-        for feature in range(num_features):
+        for feature in rootd:
             # best split val for the feature
             best_feat_split_val = self.X[0][feature]
             best_feat_split_loss = float("inf")
@@ -54,9 +60,9 @@ class Node:
         # print(losses)
         # print(len(self.X))
 
-        X_left = np.empty_like([splits])
+        X_left = np.empty_like([self.X[0]])
         y_left = np.array([])
-        X_right = np.empty_like([splits])
+        X_right = np.empty_like([self.X[0]])
         y_right = np.array([])
 
         for i in range(num_inputs):
@@ -74,8 +80,8 @@ class Node:
         if (len(y_right) == 0):
             y_right = np.append(y_right,(1 - round(np.average(self.y))))
 
-        self.left = Node(X_left[1:], y_left, self.depth + 1, self.max_depth, self.f)
-        self.right = Node(X_right[1:], y_right, self.depth + 1, self.max_depth, self.f)
+        self.left = Node(X_left[1:], y_left, self.depth + 1, self.max_depth, self.f, self.random_forests)
+        self.right = Node(X_right[1:], y_right, self.depth + 1, self.max_depth, self.f, self.random_forests)
         self.split_feature = best_split_feature
         self.split_val = splits[best_split_feature]
         return
@@ -103,13 +109,14 @@ class Node:
 
 class DecisionTree:
     #You will likely need to add more arguments to the constructor
-    def __init__(self, loss_f, max_depth=None):
+    def __init__(self, loss_f, max_depth = None, random_forests = False):
         self.max_depth = max_depth
         self.f = loss_f
+        self.random_forests = random_forests
         return
 
     def build(self, X, y):
-        self.root = Node(X, y, 0, self.max_depth, self.f)
+        self.root = Node(X, y, 0, self.max_depth, self.f, self.random_forests)
         return
     
     def predict(self, X):
@@ -168,6 +175,8 @@ y_train = np.loadtxt('data/y_train.csv', delimiter=",").astype(int)
 X_test = np.loadtxt('data/X_test.csv', delimiter=",")
 y_test = np.loadtxt('data/y_test.csv', delimiter=",").astype(int)
 
+'''
+# Part A
 missclassification_train = np.array([])
 missclassification_test = np.array([])
 gini_train = np.array([])
@@ -215,3 +224,49 @@ plt.plot(x_axis, entropy_test, label = "Entropy Test")
 plt.legend()
 plt.savefig("Entropy.png")
 plt.clf()
+'''
+# Part B
+for j in range(11):
+    print("Run:", j)
+    datasets_X = np.empty_like([X_train])
+    datasets_y = np.empty_like([y_train])
+
+    for i in range(101):
+        s_X = np.empty_like([X_train[0]])
+        s_y = np.array([])
+
+        n = len(X_train)
+        for j in range(n):
+            new = random.randint(0, n-1)
+            s_X = np.append(s_X, np.array([X_train[new]]), axis=0)
+            s_y = np.append(s_y, np.array([y_train[new]]), axis=0)
+
+        datasets_X = np.append(datasets_X, np.array([s_X[1:]]), axis=0)
+        datasets_y = np.append(datasets_y, np.array([s_y]), axis=0)
+
+    datasets_X = datasets_X[1:]
+    datasets_y = datasets_y[1:]
+    test_accuracies_without = np.array([])
+    test_accuracies_with = np.array([]) 
+    for b in range(101):
+        dtEntropyWithout = DecisionTree(entropy, 3)
+        dtEntropyWithout.build(datasets_X[b], datasets_y[b])
+        entropy_y_test = dtEntropyWithout.predict(X_test)
+        test_accuracies_without = np.append(test_accuracies_without, accuracy(entropy_y_test, y_test))
+
+        dtEntropyWith = DecisionTree(entropy, 3, True)
+        dtEntropyWith.build(datasets_X[b], datasets_y[b])
+        entropy_y_test = dtEntropyWith.predict(X_test)
+        test_accuracies_with = np.append(test_accuracies_with, accuracy(entropy_y_test, y_test))
+    
+    test_median_without = np.median(test_accuracies_without)
+    test_minimum_without = np.amin(test_accuracies_without)
+    test_maximum_without = np.amax(test_accuracies_without)
+    print("Without Random Forests")
+    print("Test Median: {}, Test Minimum: {}, Test Maximum: {}".format(test_median_without, test_minimum_without, test_maximum_without))
+
+    test_median_with = np.median(test_accuracies_with)
+    test_minimum_with = np.amin(test_accuracies_with)
+    test_maximum_with = np.amax(test_accuracies_with)
+    print("With Random Forests")
+    print("Test Median: {}, Test Minimum: {}, Test Maximum: {}".format(test_median_with, test_minimum_with, test_maximum_with))
